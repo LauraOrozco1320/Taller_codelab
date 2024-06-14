f1d4a44da33308856fae6d2c2bfa05db82bc3923
LauraOrozco1320 <orozcolauracamila@gmail.com>
2024-06-14 11:02:03 -0500

    commit nuevo

--- src/main/kotlin/Codelab-4.kt
+++ src/main/kotlin/Codelab-4.kt
@@ -1,3 +1,24 @@
-fun main() {
+//Define clase
+open class SmartDevice (val name: String, val category: String){
 
+   /* val name = "Android TV"
+    val category = "Entertainment"
+    var deviceStatus = "online"
+
+    fun turnOn() {
+        println("Smart device is turned on.")
+    }
+    fun turnOff() {
+        println("Smart device is turned off.")
+    }
+
+    */
+}
+
+fun main() {
+    /*val smartTvDevice = SmartDevice()
+    println("Device name is: ${smartTvDevice.name}")
+    smartTvDevice.turnOn()
+    smartTvDevice.turnOff()
+     */
 }
