cc3cb24e9301fa51fe830fba9f6fd337169bb570
LauraOrozco1320 <orozcolauracamila@gmail.com>
2024-06-20 11:46:52 -0500

    commit nuevo 20-06-2024 nuevo 11:46

---
+++ src/main/kotlin/practica1.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    val weekDays = arrayOf("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo")
+    println(weekDays.get(0))
+    println(weekDays.get(1))
+    println(weekDays.get(2))
+    println(weekDays.get(3))
+    println(weekDays.get(4))
+    println(weekDays.get(5))
+    println(weekDays.get(6))
+
+
+
+    if(weekDays.size >= 8){
+        println(weekDays.get(7))
+    }else{
+        println("No tiene más parámetros la array")
+    }
+}
